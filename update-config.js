c20eedb4db94e788bc077fd83ae51d806e44cbdc
NavegarHacker <navegarhacker@gmail.com>
2026-04-29 09:14:45 -0300

    Correção v2.0.3: Sincronização de versão e data interna

--- update-config.js
+++ update-config.js
@@ -1,6 +1,6 @@
 window.APP_UPDATE_CONFIG = {
-  currentVersion: '2.0.2',
-  expirationDate: '28/04/2026',
+  currentVersion: '2.0.3',
+  expirationDate: '29/04/2026',
   bundleManifestUrl: 'https://raw.githubusercontent.com/WSPREDADOR/controle-financeiro/main/update/web-manifest.json',
   bundleManifestFallbackUrl: 'https://cdn.jsdelivr.net/gh/WSPREDADOR/controle-financeiro@main/update/web-manifest.json',
   releaseApiUrl: 'https://api.github.com/repos/WSPREDADOR/controle-financeiro/releases/latest',
