fe71355211098b88440a46997f8e395283974992
NavegarHacker <navegarhacker@gmail.com>
2026-04-29 00:18:15 -0300

    Release v1.9.9: Atualização completa do app e APK

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const VERSION = '1.9.8';
+const VERSION = '1.9.9';
 
 function bundle() {
   const htmlPath = path.join(__dirname, 'index.html');
