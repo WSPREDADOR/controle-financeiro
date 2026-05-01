dccaaedb4875254bdeba8b3e09881631ef9f4ac1
NavegarHacker <navegarhacker@gmail.com>
2026-05-01 15:11:21 -0300

    Release v2.3.1

--- update-config.js
+++ update-config.js
@@ -1,5 +1,5 @@
 window.APP_UPDATE_CONFIG = {
-  currentVersion: '2.3.0',
+  currentVersion: '2.3.1',
   expirationDate: '01/05/2026',
   bundleManifestUrl: 'https://raw.githubusercontent.com/WSPREDADOR/controle-financeiro/main/update/web-manifest.json',
   bundleManifestFallbackUrl: 'https://cdn.jsdelivr.net/gh/WSPREDADOR/controle-financeiro@main/update/web-manifest.json',
