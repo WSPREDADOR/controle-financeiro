7fca1c79b52d459ae568cc3973f3eabb56359cd4
NavegarHacker <navegarhacker@gmail.com>
2026-05-09 13:27:49 -0300

    Release v2.3.13

--- script.js
+++ script.js
@@ -590,7 +590,7 @@ const Storage = {
   }
 };
 const defaultUpdateConfig = {
-  currentVersion: '2.3.12',
+  currentVersion: '2.3.13',
   bundleManifestUrl: 'https://raw.githubusercontent.com/WSPREDADOR/controle-financeiro/main/update/web-manifest.json',
   bundleManifestFallbackUrl: 'https://cdn.jsdelivr.net/gh/WSPREDADOR/controle-financeiro@main/update/web-manifest.json',
   releaseApiUrl: 'https://api.github.com/repos/WSPREDADOR/controle-financeiro/releases/latest',
